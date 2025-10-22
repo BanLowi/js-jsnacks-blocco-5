8832a6344e67a8349dd02968370be29eef145fce
BanLowi <lowi40k@gmail.com>
2025-10-22 14:58:32 +0200

    Jsnack1: forEach + for cycle

---
+++ snack-1/script.js
@@ -0,0 +1,13 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+names.forEach((name) => {
+    console.log(name);
+})
+
+for (let i = 0; i < names.length; i++) {
+    const singleName = names[i];
+    console.log(singleName);
+}
+
