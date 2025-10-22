27efd28127d1e8b77ae9a4613d954e884ea36206
BanLowi <lowi40k@gmail.com>
2025-10-22 15:16:18 +0200

    Jsnack4: for Each cycle

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,8 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+const postsAuthor = [];
+posts.forEach((post) => {
+  postsAuthor.push(post.author);
+})
+console.log(postsAuthor);
