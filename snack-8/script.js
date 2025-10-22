79e37c0536878cb07387e67fe3a2ec55ea69cc63
BanLowi <lowi40k@gmail.com>
2025-10-22 15:56:26 +0200

    Jsnack8: find cycle

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+const idOneClass = students.find((student) => {
+  if(student.id == 1) {
+    return console.log(student.class); 
+  }
+})
+
